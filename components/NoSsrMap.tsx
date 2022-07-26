e2301521bdb28ab05e4cb1d35972659d21f4980a
Jordan <1338914+Joroze@users.noreply.github.com>
2022-07-26 18:26:03 -0400

    Add to separate repo

--- components/NoSsrMap.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const MapWithNoSSR = dynamic(() => import('../components/Map'), {
-  ssr: false,
-  loading: () => <p>A map is loading</p>,
-});
-
-export default MapWithNoSSR;
