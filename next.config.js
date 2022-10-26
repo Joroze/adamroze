59c93634ff4f7e2596e6dcb24417ff78b0bd44f6
Jordan Rosenberg <1338914+Joroze@users.noreply.github.com>
2022-10-25 21:03:26 -0400

    Update nextjs, add turborepo

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
+  experimental: {
+    adjustFontFallbacks: true,
+  },
   images: {
     domains: ['loremflickr.com'],
     remotePatterns: [
